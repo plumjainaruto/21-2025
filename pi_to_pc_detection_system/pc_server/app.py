ea174dc94196557ff8173105b5275d21b9e118ba
plumjainaruto <157878306+plumjainaruto@users.noreply.github.com>
2025-04-18 20:10:48 +0700

    Update app.py

--- pi_to_pc_detection_system/pc_server/app.py
+++ pi_to_pc_detection_system/pc_server/app.py
@@ -35,6 +35,6 @@ def upload():
     })
 
     return jsonify({'status': 'success'})
-
+*************** อันนี้ต้องเอ่าไปแก้เอง**********************
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
